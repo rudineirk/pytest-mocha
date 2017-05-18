53517b26c7d21a2532abdc2a83eb796e0f73e3b0
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2017-05-17 23:02:53 -0300

    Complete python package information

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 with open('README.md') as f:
@@ -5,15 +6,34 @@ with open('README.md') as f:
 
 setup(
     name='pytest-mocha',
-    packages=['pytest_mocha'],
-    version='0.0.1',
-    entry_points={
-        'pytest11': ['pytest_mocha = pytest_mocha.plugin']
-    },
+    version='0.1.0',
     description='pytest plugin to display test execution output like a mochajs',
     long_description=long_description,
     author='Rudinei Goi Roecker',
     author_email='rudinei.roecker@gmail.com',
     url='https://github.com/rudineirk/pytest-mocha',
-    license='MIT'
+    license='MIT',
+    packages=['pytest_mocha'],
+    install_requires=[
+        'colorama>=0.3.9',
+    ],
+    entry_points={
+        'pytest11': ['pytest_mocha = pytest_mocha.plugin']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
 )
